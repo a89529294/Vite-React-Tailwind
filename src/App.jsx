ccf0c8452afba6b324b4e8f1367615a6094e3a3d
Albert Chang <a89529294@hotmail.com>
2021-08-27 10:49:52 +0800

    first

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <div className="h-[101px] w-10 bg-black">hi</div>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./index.html", "./src/**/*.{jsx,js}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
